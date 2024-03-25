3190380fac7dc6d94023dc3867140178dc96d2af
Olga Pankrashina <olyaolya2713@yandex.ru>
2024-03-25 18:03:01 +0300

    feat: add servises with subsctiption

---
+++ src/components/Services/ServicesSubscription/ServicesSubscriptionCard/ServicesSubscriptionCard.tsx
@@ -0,0 +1,86 @@
+import { Box, Card, CardMedia, Typography } from '@mui/material';
+import TransitionIcon from '../../../../assets/TransitionIcon.svg?react';
+import { useNavigate } from 'react-router-dom';
+import {
+  ServicesSubscriptionData,
+  ServicesSubscriptionImageData
+} from '../../../../utils/constants';
+
+export default function ServicesSubscriptionCard() {
+  const navigate = useNavigate();
+  const styles = {
+    container: {
+      padding: '16px 12px 16px 20px',
+      marginTop: '19px',
+      display: 'flex',
+      flexDirection: 'column',
+      border: '.4px solid #E2E2E2',
+      borderRadius: '16px',
+      boxShadow: '2px 2px 6px 2px rgba(0, 0, 0, 0.08)'
+    },
+    containerImage: { display: 'flex', marginTop: '12px' },
+    containerTitle: { display: 'flex', justifyContent: 'space-between' },
+    containerSum: { display: 'flex', marginTop: '8px' },
+    image: { width: '40px', height: '40px' },
+    title: {
+      fontFamily: 'Inter',
+      fontSize: '16px',
+      fontWeight: '400',
+      lineHeight: '1.25',
+      color: '#489865'
+    },
+
+    subtitle: {
+      textAlign: 'center',
+      fontFamily: 'Inter',
+      fontSize: '14px',
+      fontWeight: '400',
+      lineHeight: '1.25',
+      color: '#676879'
+    },
+    sum: {
+      textAlign: 'center',
+      fontFamily: 'Inter',
+      fontSize: '14px',
+      fontWeight: '400',
+      lineHeight: '1.25',
+      color: '#676879'
+    }
+  };
+
+  const handleClick = () => {
+    navigate('/subscriptions');
+  };
+  return (
+    <Card sx={styles.container}>
+      <Box sx={styles.containerTitle}>
+        <Typography component="p" sx={styles.title}>
+          {ServicesSubscriptionData.title}
+        </Typography>
+        <TransitionIcon onClick={handleClick} />
+      </Box>
+      <Box sx={styles.containerImage}>
+        {ServicesSubscriptionImageData.card.map((data, index) => (
+          <CardMedia
+            key={index}
+            component="img"
+            sx={{
+              ...styles.image,
+              marginLeft: index === 0 ? '0' : '-8px'
+            }}
+            image={data.image}
+            alt={data.alt}
+          />
+        ))}
+      </Box>
+      <Box sx={styles.containerSum}>
+        <Typography component="p" sx={styles.subtitle}>
+          {ServicesSubscriptionData.writeOff}
+        </Typography>
+        <Typography component="p" sx={styles.sum}>
+          &nbsp;{ServicesSubscriptionData.sum}&#8381;
+        </Typography>
+      </Box>
+    </Card>
+  );
+}
---
+++ src/components/Services/ServicesSubscription/ServicesSubscriptionHistory/ServicesSubscriptionHistory.tsx
@@ -0,0 +1,83 @@
+import { Box, Card, Typography } from '@mui/material';
+import TransitionIcon from '../../../../assets/TransitionIcon.svg?react';
+import { useNavigate } from 'react-router-dom';
+import { ServicesSubscriptionHistoryData } from '../../../../utils/constants';
+
+export default function ServicesSubscriptionHistory() {
+  const navigate = useNavigate();
+  const styles = {
+    container: {
+      padding: '16px 12px 16px 20px',
+      marginTop: '32px',
+      display: 'flex',
+      flexDirection: 'column',
+      border: '.4px solid #E2E2E2',
+      borderRadius: '16px',
+      boxShadow: '2px 2px 6px 2px rgba(0, 0, 0, 0.08)'
+    },
+    containerTitle: { display: 'flex', justifyContent: 'space-between' },
+    containerSum: { display: 'flex', justifyContent: 'space-between', marginTop: '8px' },
+    image: { width: '40px', height: '40px' },
+    title: {
+      fontFamily: 'Inter',
+      fontSize: '16px',
+      fontWeight: '400',
+      lineHeight: '1.25',
+      color: '#489865',
+      marginBottom: '4px'
+    },
+
+    subtitle: {
+      fontFamily: 'Inter',
+      fontSize: '14px',
+      fontWeight: '400',
+      lineHeight: '1.25'
+    },
+    sum: {
+      textAlign: 'center',
+      fontFamily: 'Inter',
+      fontSize: '14px',
+      fontWeight: '700',
+      lineHeight: '1.25',
+      color: '#168E2C'
+    },
+    sumMinus: {
+      textAlign: 'center',
+      fontFamily: 'Inter',
+      fontSize: '14px',
+      fontWeight: '700',
+      lineHeight: '1.25',
+      color: '#676879'
+    }
+  };
+
+  const handleClick = () => {
+    navigate('/sell_history');
+  };
+  return (
+    <Card sx={styles.container}>
+      <Box sx={styles.containerTitle}>
+        <Typography component="p" sx={styles.title}>
+          {ServicesSubscriptionHistoryData.title}
+        </Typography>
+        <TransitionIcon onClick={handleClick} />
+      </Box>
+      <Box sx={styles.containerSum}>
+        <Typography component="p" sx={styles.subtitle}>
+          {ServicesSubscriptionHistoryData.subtitlePlus}
+        </Typography>
+        <Typography component="p" sx={styles.sum}>
+          {ServicesSubscriptionHistoryData.sumPlus} &#8381;
+        </Typography>
+      </Box>
+      <Box sx={styles.containerSum}>
+        <Typography component="p" sx={styles.subtitle}>
+          {ServicesSubscriptionHistoryData.subtitleMinus}
+        </Typography>
+        <Typography component="p" sx={styles.sumMinus}>
+          {ServicesSubscriptionHistoryData.sumMinus} &#8381;
+        </Typography>
+      </Box>
+    </Card>
+  );
+}
