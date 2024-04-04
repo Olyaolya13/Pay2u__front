ec7cfd44dc560851ce5c7e410d2c17258a48f6de
Olga Pankrashina <olyaolya2713@yandex.ru>
2024-04-04 09:23:46 +0300

    ref: add value in filter select

--- src/components/Filters/FiltersPopupSelect/FiltersPopupSelect.tsx
+++ src/components/Filters/FiltersPopupSelect/FiltersPopupSelect.tsx
@@ -1,4 +1,4 @@
-import './FiltersPopupSelect.css';
+import style from './FiltersPopupSelect.module.scss';
 import { Box, List, ListItemText, MenuItem, Modal, Typography } from '@mui/material';
 import FilterPopupBtn from '../../../assets/FilterPopupBtn.svg?react';
 import CloseIcon from '../../../assets/CloseIconGreen.svg?react';
@@ -11,36 +11,36 @@ interface FiltersPopupSelectProps {
   onSelectOption: (option: string) => void;
 }
 
+const font = { fontFamily: 'Inter', fontSize: '22px', fontWeight: '700' };
+
+const styles = {
+  container: { backgroundColor: 'rgba(0, 0, 0, 0.5)' },
+  title: {
+    ...font,
+    textTransform: 'none',
+    margin: '20px 0 32px'
+  },
+  text: { display: 'flex', flexDirection: 'column', alignItems: 'center' },
+  popover: {
+    padding: '12px 20px 66px',
+    backgroundColor: '#fff',
+    borderRadius: '16px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    position: 'fixed',
+    top: '301px',
+    left: 0,
+    width: '335px',
+    height: '100%'
+  }
+};
+
 export default function FiltersPopupSelect({
   open,
   onClose,
   onSelectOption
 }: FiltersPopupSelectProps) {
-  const styles = {
-    container: { backgroundColor: 'rgba(0, 0, 0, 0.5)' },
-    title: {
-      fontFamily: 'Inter',
-      fontSize: '22px',
-      fontWeight: '700',
-      textTransform: 'none',
-      margin: '20px 0 32px'
-    },
-    text: { display: 'flex', flexDirection: 'column', alignItems: 'center' },
-    popover: {
-      padding: '12px 20px 66px',
-      backgroundColor: '#fff',
-      borderRadius: '16px',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      position: 'fixed',
-      top: '301px',
-      left: 0,
-      width: '335px',
-      height: '100%'
-    }
-  };
-
   const handleMenuItemClick = (option: string) => {
     setSelectedOption(option);
     console.log('Selected option:', option);
@@ -66,7 +66,7 @@ export default function FiltersPopupSelect({
           <FilterPopupBtn />
           <Box>
             <Typography sx={styles.title}>Сортировка</Typography>
-            <CloseIcon className="filter-popup__icon" onClick={handleClose} />
+            <CloseIcon className={style.close__icon} onClick={handleClose} />
           </Box>
         </Box>
         <List
